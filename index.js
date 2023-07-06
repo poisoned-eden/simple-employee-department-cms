95d7a8ac36768e8f1bda1bbcee30f387b1216046
poisoned-eden <poisoned.eden@gmail.com>
2023-07-06 18:12:36 +0100

    adds beginnings of logic for follow up qs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const questions = require('./questions.js');
 const mysql = require('mysql2');
+require('mysql2/promise');
 const inquirer = require('inquirer');
 
 const db = mysql.createConnection({
@@ -10,10 +11,46 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-db.connect();
-
 async function init() {
     const mainMenu = await inquirer.prompt(questions.menu);
+
+    console.log(mainMenu);
+
+    switch (mainMenu.menuChoice) {
+        case 'View All Departments':
+            viewDepts();
+            break;
+        case 'View All Roles':
+            viewRoles();
+            break;
+        case 'View All Employees':
+            viewEmployees();
+            break;
+        case 'Add A Department':
+            addDept();
+            break;
+        case 'Add A Role':
+            addRole();
+            break;
+        case 'Add An Employee':
+            addEmployee();
+            break;
+        case 'Update An Employee Role':
+            updateEmployee();
+            break;
+        default:
+            console.log('Thank you');
+    };
+};
+
+async function viewDepts() {
+    await db.query("SELECT * FROM departments;", (err, results) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.table(results);
+        };
+    });
 };
 
 init();
